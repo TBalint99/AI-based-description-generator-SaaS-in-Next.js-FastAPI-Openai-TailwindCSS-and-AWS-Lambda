50bafa160f2af12f6221f04d8f8d6c34fb927f16
Takács Bálint <tbalint99@gmail.com>
2023-01-08 14:41:42 +0100

    loading animation, framer motion button, layout fixes

--- frontend/components/BrandyForm.tsx
+++ frontend/components/BrandyForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from "framer-motion"
+import { easeInOut, motion } from "framer-motion"
 
 type Props = {
     topic: string,
@@ -15,7 +15,7 @@ const characterLimit: number = 60
 export default function BrandyForm({ topic, category, keywords, onChange, onClick, isLoading }: Props) {
   return (
     <div className='px-3 pb-3 sm:mx-3 sm:px-10 flex flex-col'>
-        <p className='mb-8 sm:text-lg text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-800'>Tell me what type of catchy text you want and I will generate you one.<br />With extra keywords!</p>
+        <p className='mb-8 sm:text-md text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-blue-800'>Tell me what type of catchy text you want and I will generate you one.<br/>With extra keywords!</p>
         <label className='font-medium text-gray-200' htmlFor='topic'>What is you business dealing with?</label>
         <input id="topic" type="text" placeholder="e.g.: local street food restaurant with juicy burgers"
             name="topic" value={topic} onChange={onChange}
@@ -42,15 +42,32 @@ export default function BrandyForm({ topic, category, keywords, onChange, onClic
             whileTap={{ scale: 0.9 }}
             onClick={onClick}
             disabled={topic.length > characterLimit}
-            className="w-3/4 mx-auto my-3 py-3 outline-none bg-gradient-to-r from-cyan-600 to-blue-800 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm"
+            className="w-3/4 mx-auto my-3 py-3 outline-none bg-gradient-to-r from-cyan-600 to-blue-800 hover:bg-blue-700 disabled:opacity-40 focus:outline-none font-medium rounded-lg text-sm"
         >Submit</motion.button>
         {
             isLoading && (
-                <motion.div
-                    className=""
-                    animate={{ width: '100%' }}
-                    transition={{ duration: 1, ease: 'easeInOut' }}
-                >Loading...</motion.div>
+                <div className='flex flex-row items-center sm:mt-3 py-2 gap-1'>
+                <p className='mr-2'>Your response is being generated...</p>
+                    {
+                        [0,1,2].map(() => (
+                            <motion.div
+                                animate={{
+                                    scale: [1, 1.5, 1.5, 1, 1],
+                                    rotate: [0, 0, 180, 180, 0],
+                                    borderRadius: ["20%", "30%", "40%", "50%", "20%"]
+                                }}
+                                transition={{
+                                    duration: 2,
+                                    ease: "easeInOut",
+                                    times: [0, 0.2, 0.5, 0.8, 1],
+                                    repeat: Infinity,
+                                    repeatDelay: 1
+                                }}
+                                className='w-4 h-4 mx-1 rounded-lg bg-gradient-to-r from-cyan-600 to-blue-800 hover:bg-blue-700'
+                            />
+                        ))
+                    }
+                </div>
             )               
         }
     </div>  
--- frontend/components/BrandyResponse.tsx
+++ frontend/components/BrandyResponse.tsx
@@ -10,11 +10,11 @@ type Props = {
 export default function BrandyResponse({ description, keywords, onClick }: Props) {
   return (
     <div className='container flex flex-col justify-center px-3 sm:pb-3 sm:mx-3 sm:px-10 mt-3'>
-        <div className='bg-slate-200 text-black rounded-lg py-3 px-4 drop-shadow-md overflow-y-scroll max-h-60 scroll-m-1'>
-          <p  className='text-md font-semibold mb-3'>Your prompt:</p>
+        <div className='bg-slate-200 text-black rounded-lg py-3 px-4 drop-shadow-md overflow-y-scroll max-h-60 scrollbar-thin scrollbar-track-slate-200 scrollbar-thumb-slate-300'>
+          <p  className='text-md font-semibold mb-3'>Your description:</p>
           <p className='font-light'>{description}</p>
         </div>
-        <div className='bg-slate-200 text-black rounded-lg mt-5 py-3 px-4 drop-shadow-md'>
+        <div className='bg-slate-200 text-black rounded-lg mt-5 py-3 px-4 drop-shadow-md overflow-y-scroll max-h-52 scrollbar-thin scrollbar-track-slate-200 scrollbar-thumb-slate-300'>
           <p  className='text-md font-semibold mb-3'>Your hashtag keywords:</p>
           <div className='flex flex-row gap-2 flex-wrap'>
             {
@@ -32,7 +32,7 @@ export default function BrandyResponse({ description, keywords, onClick }: Props
                 transition: { duration: 0.2 },
             }}
             whileTap={{ scale: 0.9 }}
-            //onClick={onClick}
+            onClick={onClick}
             className="w-2/3 md:w-2/5 mx-auto my-7 py-3 outline-none bg-gradient-to-r from-cyan-600 to-blue-800 hover:bg-blue-700 focus:outline-none font-medium rounded-lg text-sm"
         >Reset</motion.button>
     </div>
