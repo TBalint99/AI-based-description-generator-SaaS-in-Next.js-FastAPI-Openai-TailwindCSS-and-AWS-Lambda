afe6867dc4afca30af155978efe45d0bc08875e0
Takács Bálint <tbalint99@gmail.com>
2023-01-09 22:23:45 +0100

    floating and rotating bank card created with react three fiber

--- frontend/components/BrandyGlass.tsx
+++ frontend/components/BrandyGlass.tsx
@@ -1,12 +1,21 @@
 // @ts-nocheck
-import React, { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Sky } from '@react-three/drei'
+import React, { useRef, Suspense } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { ContactShadows, OrbitControls, Sky } from '@react-three/drei'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { Mesh } from 'three'
 
 const Cube = () => {
 
   const meshRef = useRef<Mesh>(null)
+
+  const [colorMap, displacementMap, normalMap, roughnessMap, metalness] = useLoader(TextureLoader, [
+    '/PaymentCard002_4K_Color.jpg',
+    '/PaymentCard002_4K_Displacement.jpg',
+    '/PaymentCard002_4K_Opacity.jpg',
+    '/PaymentCard002_4K_Roughness.jpg',
+    '/PaymentCard002_4K_Metalness.jpg',
+  ])
 
   useFrame(() => {
     if (!meshRef.current) {
@@ -17,22 +26,32 @@ const Cube = () => {
   })
 
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry attach="geometry" />
-      <meshStandardMaterial color="blue" />
+    <mesh ref={meshRef} shadow>
+      <ContactShadows opacity={1} scale={10} blur={1} far={10} resolution={256} color="#0f0f0f" />
+      <planeGeometry args={[3, 2, 10]} />
+      <meshLambertMaterial
+        displacementScale={0.2}
+        map={colorMap}
+        displacementMap={displacementMap}
+      />
     </mesh>
   )
 }
 
 export default function BrandyGlass() {
   return (
-    <div className='h-screen'>
-      <Canvas>
-        <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Cube />
-      </Canvas>
+    <div className='h-screen bg-slate-500 z-0'>
+      <div className='z-10 text-center'>
+        <p className=''>Ready for growing your bank account?</p>
+      </div>
+      <Suspense fallback={null}>
+        <Canvas background='black'>
+          <ambientLight intensity={0.2} />
+          <directionalLight />
+          <spotLight position={[10, 15, 10]} angle={0.3} />
+          <Cube />
+        </Canvas>
+      </Suspense>
     </div>
   )
 }
